a1ee02d6da8f6473cadea6501e0af462c0c0f06d
Choudareddy Medam <choudareddymedam901@gmail.com>
2026-04-29 07:52:57 +0530

    Add files via upload
    Day - 01 Notes and Lab

---
+++ Day-01_Notes.py
@@ -0,0 +1,75 @@
+Python: -
+=========
+github --> choudareddymedam901
+https://github.com/choudareddymedam901/Python.git
+
+jupyter notebook
+
+Need to download in .ipynb format
+
+
+By Using Python Programming we can develop
+
+1. System programmming
+2. GUI Interface
+3. Web Scrapping
+4. DB
+5. Num / Sci Programming
+6. Game Developement
+7. Image Processing
+8. Robotics 
+etc.,. (many more)
+
+why we are using python?
+
+--> it is easy to use.
+--> build-in libraries
+--> It supports every libraries
+--> easy configuration
+
+Python is available in 2.x and 3.x 
+
+What is variable?
+-----------------
+
+Variable is used to store the data. for which we can use that in the later stage of the programming.
+
+
+While creating the variable we need to give type of data that is data type.
+
+But in python it is dynamically typed programming language so we are not going to give any type of the variable.
+
+if we want to know the type of the variable then we can use the method 
+
+type(variable)
+
+Data Types: -
+--------------
+
+int
+float
+str
+complex (real and imaginary values)
+bool
+
+
+if we are using the same reference then we will get the updated values
+
+eg:- 
+
+a=10
+a=100
+100 will be updated value.
+
+
+if we want to create multiple variables at a time then...
+
+a,b,c,d,e = 1,"abc",3+2j,True,234.213
+
+We are trying to add string with integer then it will throw an error
+if fetch this we need type cast the variable before we use.
+
+str() --> function to type cast any of the type into string.
+
+input() --> if i want to take input from the user then we can use input function.
+
