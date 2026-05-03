91d610c47eb53a3c06ef9d734f1beac33a9fd548
Choudareddy Medam <choudareddymedam901@gmail.com>
2026-05-03 13:19:40 +0530

    Add files via upload

---
+++ Day-2_Notes.py
@@ -0,0 +1,59 @@
+Day-02_Notes: -
+===============
+
+string Data Type: -
+-------------------
+
+string indexing (it may use in list and ranges also)
+
+a[start:stop:step]
+
+len(a)
+
+try +, *, -, / between two strings.
+
+string methods:
+
+find()
+count()
+split()
+upper()
+swapcase()
+title()
+capitalize()
+
+title() vs capitalize()
+
+join()
+
+rstrip()
+lstrip()
+isupper()
+islower()
+isspace()
+
+isdigit()
+endswith()
+startswith()
+istitle()
+
+
+
+list: -
+========
+
+append()
+pop()
+in
+insert()
+reverse()
+count()
+extend()
+
+append() vs extend()
+
+
+
+write a program on Reverse of a string ?
+
+Medam Venkata Rama Choudareddy 
