7661caad15c7a8285f8f2e956af15b3c3ebad32c
Choudareddy Medam <choudareddymedam901@gmail.com>
2026-05-07 05:26:19 +0530

    Add files via upload

---
+++ Day-03_Notes.py
@@ -0,0 +1,30 @@
+Day-03_Notes: -
+===============
+
+tuple: -
+======
+
+it is immutlable
+
+two menthods
+
+1. count()
+2. index()
+
+set: -
+======
+
+no duplicate values.
+it is a collection of all unique values.
+it is unordered collection.
+slicing will not work.
+
+
+set(list1)
+
+remove()
+discard()
+
+dictionary:-
+============
+
